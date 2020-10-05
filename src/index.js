b63177ba96b1d8d96938c6775cf0f8611bafa78c
Rozhkow <70947620+Rozhkow@users.noreply.github.com>
2020-10-06 00:11:43 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j][0] === str[i]) {
+        if (result.length!=0 && result[result.length - 1] === bracketsConfig[j][0] && bracketsConfig[j][1] === str[i]) {
+          result = result.slice(0, result.length - 1);
+        } else {
+          result += str[i];
+        }
+        
+      } else if (bracketsConfig[j][1] === str[i] ) {
+         if (result[result.length - 1] === bracketsConfig[j][0] && bracketsConfig[j][1] === str[i]) {
+           result = result.slice(0, result.length - 1);
+         } else if (result[result.length - 1] !== bracketsConfig[j][0]) {
+           return false;
+         }
+      }
+    }
+  }
+
+  if (result.length!=0) {
+    return false;
+  }
+
+  return true;
 }
